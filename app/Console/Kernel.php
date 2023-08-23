8bea7c71dfc228caeb075772be90f66da89546ca
kevinfadhil <142968999+kevinfadhil@users.noreply.github.com>
2023-08-23 14:05:24 +0700

    Tugas TPEC - 10518157 Kevin

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -11,8 +11,7 @@ class Kernel extends ConsoleKernel
      * Define the application's command schedule.
      */
     protected function schedule(Schedule $schedule): void
-    {
-        // $schedule->command('inspire')->hourly();
+    {// $schedule->command('inspire')->hourly();
     }
 
     /**
